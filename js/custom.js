bf8ca74dd9be48e1913996faca42e70e98026d0c
rahulchakma <rahulchakma221@gmail.com>
2021-03-27 16:49:51 +0600

    boom

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+$(function(){
+
+  $('.all-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed:1000,
+    // centerMode:true,
+    centerPadding:false,
+    vertical:true,
+    arrows:true,
+    prevArrow: '.top',
+    nextArrow:'.bottom',
+  });
+
+  // testimonial
+
+
+});
